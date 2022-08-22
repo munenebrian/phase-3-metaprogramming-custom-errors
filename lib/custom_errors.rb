0fe4371ae3f1a2443b953669ab61f2a23af5a136
munenebrian <njerubrianmunene@gmail.com>
2022-08-22 11:27:57 +0300

    commit tree 3e084cd56ac127249115c0c3a98711b9f6af85d6

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -2,14 +2,31 @@ class Person
   attr_accessor :partner, :name
 
   def initialize(name)
+
     @name = name
+
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+
+    if person.is_a?(Person)
+      self.partner = person
+      person.partner = self
+    else
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    end
+
   end
 
+  class PartnerError < StandardError
+    def message
+      "you should get married!"
+    end
+  end
 end
 
 beyonce = Person.new("Beyonce")
